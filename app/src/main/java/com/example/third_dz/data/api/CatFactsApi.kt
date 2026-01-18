453a6fd35249076b27c00c1f287a400dbea52c81
yuliaperevalova <yulia.perevalova.04@gmail.com>
2026-01-19 02:22:43 +1000

    Enhance CatFactsApi to include language parameter for fetching facts; update getRandomFacts and getFactById methods to support language-specific queries.

--- app/src/main/java/com/example/third_dz/data/api/CatFactsApi.kt
+++ app/src/main/java/com/example/third_dz/data/api/CatFactsApi.kt
@@ -7,11 +7,13 @@ import retrofit2.http.Query
 interface CatFactsApi {
     @GET("/")
     suspend fun getRandomFacts(
-        @Query("count") count: Int = 20
+        @Query("count") count: Int = 20,
+        @Query("lang") lang: String = "rus-ru"
     ): MeowFactsResponse
 
     @GET("/")
     suspend fun getFactById(
-        @Query("id") id: Int
+        @Query("id") id: Int,
+        @Query("lang") lang: String = "rus-ru"
     ): MeowFactsResponse
 }
