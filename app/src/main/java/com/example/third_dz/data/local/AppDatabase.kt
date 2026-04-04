2e8b66f3d1cf06c26b56ace56f87dc9a9dcc3fc1
yuliaperevalova <yulia.perevalova.04@gmail.com>
2026-04-05 00:59:10 +1000

    add FilmEntity to AppDatabase, bump version to 2

--- app/src/main/java/com/example/third_dz/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/third_dz/data/local/AppDatabase.kt
@@ -3,7 +3,11 @@ package com.example.third_dz.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavouriteFilmEntity::class], version = 1)
+@Database(
+    entities = [FavouriteFilmEntity::class, FilmEntity::class],
+    version = 2
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favouriteFilmDao(): FavouriteFilmDao
+    abstract fun filmDao(): FilmDao
 }
